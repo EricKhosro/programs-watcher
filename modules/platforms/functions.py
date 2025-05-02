20663718132f8c9d60d31eb172456da8972ca277
Eric Khosrafian <erickhosrafian.com>
2025-05-02 19:27:53 +0330

    modify check_send_notification

--- modules/platforms/functions.py
+++ modules/platforms/functions.py
@@ -28,37 +28,38 @@ def save_data(db, platformName, programKey, dataJson):
         '$set': dataJson}, upsert=True)
 
 def check_send_notification(first_time, is_update, data, watcherData, monitor, notifications):
-    pt_notify_status = False
-    notify_status = False
+    return not first_time
+    # pt_notify_status = False
+    # notify_status = False
 
-    # Check if the program is in the specific programs list
-    # if watcherData['programURL'] in monitor['specific_programs']:
-    #     pt_notify_status = True  # Allow notification if it's a specific program
+    # # Check if the program is in the specific programs list
+    # # if watcherData['programURL'] in monitor['specific_programs']:
+    # #     pt_notify_status = True  # Allow notification if it's a specific program
 
-    # Monitor program types (RDP and VDP)
-    if data['isNewProgram']:
-        if data['programType'] == "rdp" and monitor['rdp']:
-            pt_notify_status = True
-        elif data['programType'] == "vdp" and monitor["vdp"]:
-            pt_notify_status = True
-    else:
-        if watcherData['programType'] == "rdp" and monitor['rdp']:
-            pt_notify_status = True
-        elif watcherData['programType'] == "vdp" and monitor["vdp"]:
-            pt_notify_status = True   
+    # # Monitor program types (RDP and VDP)
+    # if data['isNewProgram']:
+    #     if data['programType'] == "rdp" and monitor['rdp']:
+    #         pt_notify_status = True
+    #     elif data['programType'] == "vdp" and monitor["vdp"]:
+    #         pt_notify_status = True
+    # else:
+    #     if watcherData['programType'] == "rdp" and monitor['rdp']:
+    #         pt_notify_status = True
+    #     elif watcherData['programType'] == "vdp" and monitor["vdp"]:
+    #         pt_notify_status = True   
 
-    # Exclude program URLs
-    if watcherData['programURL'] in monitor['excluded_programs']:
-        print(f"Program {watcherData['programURL']} is excluded from notifications")
-        return False  # Block notifications early
+    # # Exclude program URLs
+    # if watcherData['programURL'] in monitor['excluded_programs']:
+    #     print(f"Program {watcherData['programURL']} is excluded from notifications")
+    #     return False  # Block notifications early
 
-    # Check the notification conditions
-    if pt_notify_status:
-        # First-time setup should not trigger notifications unless updates exist
-        if not first_time:
-            if data['isNewProgram'] and notifications['new_program']:
-                notify_status = True
-            elif is_update and not data['isNewProgram']:
-                notify_status = True
+    # # Check the notification conditions
+    # if pt_notify_status:
+    #     # First-time setup should not trigger notifications unless updates exist
+    #     if not first_time:
+    #         if data['isNewProgram'] and notifications['new_program']:
+    #             notify_status = True
+    #         elif is_update and not data['isNewProgram']:
+    #             notify_status = True
 
-    return notify_status
+    # return notify_status
